7d8837fbf28b90cd6409e65b412b823218923b90
RiccardoDeZen <riccardo.dezen@studenti.unipd.it>
2019-10-15 22:41:50 +0200

    Started implementing fragment

--- app/src/main/java/com/dezen/riccardo/musicplayer/MainActivity.java
+++ app/src/main/java/com/dezen/riccardo/musicplayer/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.action_play_group:
-
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
--- app/src/main/java/com/dezen/riccardo/musicplayer/SongListFragment.java
+++ app/src/main/java/com/dezen/riccardo/musicplayer/SongListFragment.java
@@ -1,14 +1,84 @@
 package com.dezen.riccardo.musicplayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 
+import java.util.List;
+
 public class SongListFragment extends Fragment{
 
+    private SongViewModel songList;
+    private ListView songsListView;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        songList = ViewModelProviders.of(this).get(SongViewModel.class);
+        songList.getSongList().observe(this, new Observer<List<Song>>() {
+            @Override
+            public void onChanged(List<Song> songs) {
+                songsListView.setAdapter(new CustomAdapter());
+            }
+        });
+        songsListView = getView().findViewById(R.id.songs_listview);
+        songsListView.setAdapter(new CustomAdapter());
+        mediaPlayer = new MediaPlayer();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_songlist, container, false);
+    }
+
+    private View getItemView(final int index){
+        final View newView = getLayoutInflater().inflate(R.layout.song_listview_item, null);
+        ((TextView)newView.findViewById(R.id.textView_song_title)).setText(songList.get(index).getTitle());
+        newView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //play(songList.get(index).getData());
+            }
+        });
+        return newView;
+    }
+
+    private class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return songList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return getItemView(position);
+        }
+    }
 }
